8bd619e9177a0c24e72c1707c8106910032d0d66
Mohsin <uamohsin.5@gmail.com>
2025-04-03 20:56:49 +0530

    .

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,49 +1,41 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Collections;
 
 public class LevelManager : MonoBehaviour
 {
     public Button[] levelButtons; // Assign buttons in Inspector
+    public AudioSource buttonClickSound; // Assign in Inspector
 
- void Start()
-{
-    int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
-
-    for (int i = 0; i < levelButtons.Length; i++)
+    void Start()
     {
-        if (i + 1 <= unlockedLevel)
-        {
-            levelButtons[i].interactable = true;
-            int levelIndex = i + 1; 
+        int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
 
-            levelButtons[i].onClick.RemoveAllListeners(); // Clear old events
-            levelButtons[i].onClick.AddListener(() => LoadLevel(levelIndex));
-        }
-        else
+        for (int i = 0; i < levelButtons.Length; i++)
         {
-            levelButtons[i].interactable = false;
+            if (i + 1 <= unlockedLevel)
+            {
+                levelButtons[i].interactable = true;
+                int levelIndex = i + 1; 
+
+                levelButtons[i].onClick.RemoveAllListeners(); // Clear old events
+                levelButtons[i].onClick.AddListener(() => StartCoroutine(PlaySoundAndLoad(levelIndex)));
+            }
+            else
+            {
+                levelButtons[i].interactable = false;
+            }
         }
     }
-}
 
-
-
- public void LoadLevel(int levelIndex)
-{
-    Debug.Log("Button Clicked! Trying to load Level: " + levelIndex);
-
-    if (SceneManager.GetSceneByName("Level" + levelIndex) != null)
+    IEnumerator PlaySoundAndLoad(int levelIndex)
     {
-        Debug.Log("Scene Exists: Loading Level " + levelIndex);
+        if (buttonClickSound != null)
+            buttonClickSound.Play(); // Play sound before loading
+
+        yield return new WaitForSeconds(buttonClickSound.clip.length); // Wait for the sound to finish
+
         SceneManager.LoadScene("Level" + levelIndex);
     }
-    else
-    {
-        Debug.LogError("Scene Level" + levelIndex + " is NOT in Build Settings!");
-    }
-}
-
-
-    
 }
